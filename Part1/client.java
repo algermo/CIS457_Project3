b2d6193979adc90d050fbe73c07618f627bd6e92
algermo <algermo@mail.gvsu.edu>
2016-11-02 13:19:41 -0400

    client threads are now working

--- Part1/client.java
+++ Part1/client.java
@@ -25,35 +25,38 @@ class client {
         System.out.println("Enter a username: ");
 		username = inFromUser.readLine();
 		outToServer.writeBytes("u " + username + "\n");
-		while(true) {
-			Runnable out = new OutputHandler(clientSocket);
-			Runnable in = new InputHandler(clientSocket);
-			Thread outputThread = new Thread(out);
-			Thread inputThread = new Thread(in);
-			outputThread.start();
-			inputThread.start();
-		}
+
+		Runnable out = new OutputHandler(clientSocket);
+		Runnable in = new InputHandler(clientSocket);
+		Thread outputThread = new Thread(out);
+		Thread inputThread = new Thread(in);
+		outputThread.start();
+		inputThread.start();
+		
 	}
 }
 
 class OutputHandler implements Runnable {
 	
 	Socket clientSocket;
-	ClientHandler(Socket connection) {
+	OutputHandler(Socket connection) {
 		clientSocket = connection;
 	}
 	
 	public void run() {
-	
-		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-	
-		while(true){
-				
-			System.out.println("Enter a message: ");
-			String message = inFromUser.readLine();
-			outToServer.writeBytes(message + '\n');
-				
+		try {
+			BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+			DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		
+			while(true){
+					
+				System.out.println("Enter a message: ");
+				String message = inFromUser.readLine();
+				outToServer.writeBytes(message + '\n');
+					
+			}
+		} catch (Exception e) {
+			System.out.println("Something when wrong in output run. \n");
 		}
 	}
 	
@@ -69,22 +72,27 @@ class InputHandler implements Runnable {
 			+ "h lists this set of commands again \n";
 	
 	Socket clientSocket;
-	ClientHandler(Socket connection) {
+	InputHandler(Socket connection) {
 		clientSocket = connection;
 	}
 	
 	public void run() {
 	
-		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		try {
 	
-		while(true){
-				
-			String recvMessage = inFromServer.readLine();
-			if(recvMessage.equals("help")) {
-				System.out.println(cmd);
-			} else {
-				System.out.println(recvMessage);
+			BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		
+			while(true){
+					
+				String recvMessage = inFromServer.readLine();
+				if(recvMessage.equals("help")) {
+					System.out.println(cmd);
+				} else {
+					System.out.println(recvMessage);
+				}
 			}
+		} catch (Exception e) {
+			System.out.println("Something when wrong in input run. \n");
 		}
 	}
 	
